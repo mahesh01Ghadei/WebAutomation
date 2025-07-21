5770b01bd756504619d0ab88b510b5960a5ba9eb
Mahesh <109301684+mahesh01Ghadei@users.noreply.github.com>
2025-07-21 22:36:33 +0530

    Update AJAXComponentTest.java

--- src/test/java/UITesting/AJAXComponentTest.java
+++ src/test/java/UITesting/AJAXComponentTest.java
@@ -16,7 +16,7 @@ public class AJAXComponentTest {
 		base.getDriver().findElement(By.id("ajaxButton")).click();
 		
 		WebDriverWait ajaxElementWait = new WebDriverWait(base.getDriver(), Duration.ofSeconds(20));
-		Boolean isAjaxDataLoaded = wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[@id='content']//p"), "Data loaded with AJAX get request."));
+		Boolean isAjaxDataLoaded = ajaxElementWait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[@id='content']//p"), "Data loaded with AJAX get request."));
 
 	    System.out.println(isAjaxDataLoaded? "AJAX data fetched successfully":"Failed to fetch AJAX data");
 			
