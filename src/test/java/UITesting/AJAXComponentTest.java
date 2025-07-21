35f1ca44c715c9aa9379b07e19440cc830bd8ec1
Mahesh <109301684+mahesh01Ghadei@users.noreply.github.com>
2025-07-21 22:32:52 +0530

    Update AJAXComponentTest.java

--- src/test/java/UITesting/AJAXComponentTest.java
+++ src/test/java/UITesting/AJAXComponentTest.java
@@ -7,9 +7,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AJAXComponentTest {
-
-
-	
 	public static void main(String [] args) {
 		
         BaseClass base = new BaseClass();
