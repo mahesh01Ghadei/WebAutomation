0e6e02c3eabd8129afae95712f28dd88f6073583
Mahesh <109301684+mahesh01Ghadei@users.noreply.github.com>
2025-07-21 16:16:40 +0530

    Update AJAXComponentTest.java

--- src/test/java/UITesting/AJAXComponentTest.java
+++ src/test/java/UITesting/AJAXComponentTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AJAXComponentTest {
 
-	public static void main(String [] arg) {
+	public static void main(String [] args) {
 		
 		BaseClass base = new BaseClass();
         base.setup();
@@ -16,10 +16,10 @@ public class AJAXComponentTest {
         base.getDriver().findElement(By.xpath("//a[text()='AJAX Data']")).click();
 		base.getDriver().findElement(By.id("ajaxButton")).click();
 		
-		WebDriverWait waitt = new WebDriverWait(base.getDriver(), Duration.ofSeconds(20));
-		Boolean lbl = wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[@id='content']//p"), "Data loaded with AJAX get request."));
+		WebDriverWait ajaxElementWait = new WebDriverWait(base.getDriver(), Duration.ofSeconds(20));
+		Boolean isAjaxDataLoaded = wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[@id='content']//p"), "Data loaded with AJAX get request."));
 
-	    System.out.println(lbl? "AJAX data fetched successfully":"Failed to fetch AJAX data");
+	    System.out.println(isAjaxDataLoaded? "AJAX data fetched successfully":"Failed to fetch AJAX data");
 			
 	    base.tearDown();
 		
